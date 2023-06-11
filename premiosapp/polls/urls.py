2e5632dc268d719c22ea5f56a442dc5fb9823946
JeffersonAhimar <jeffersonahimar@outlook.com>
2023-06-10 22:44:30 -0500

    first views

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/detail/5
+    path("detail/<int:question_id>", views.detail, name="detail"),
+    # ex: /polls/results/5
+    path("results/<int:question_id>", views.results, name="results"),
+    # ex: /polls/vote/5
+    path("vote/<int:question_id>", views.vote, name="vote")
 ]
