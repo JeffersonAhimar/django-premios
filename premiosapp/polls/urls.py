294d0b0c3796a795f1e8c293d1d5bfa1f5f3064c
JeffersonAhimar <jeffersonahimar@outlook.com>
2023-06-11 11:23:34 -0500

    generic views

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # ex: /polls/detail/5
-    path("detail/<int:question_id>", views.detail, name="detail"),
+    path("detail/<int:pk>", views.DetailView.as_view(), name="detail"),
     # ex: /polls/results/5
-    path("results/<int:question_id>", views.results, name="results"),
+    path("results/<int:pk>", views.ResultView.as_view(), name="results"),
     # ex: /polls/vote/5
     path("vote/<int:question_id>", views.vote, name="vote")
 ]
